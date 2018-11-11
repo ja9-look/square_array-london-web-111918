fdd2468ecfa6b7c713c1005690376ea7b488725b
Janine Luk <janineluk95@gmail.com>
2018-11-11 09:41:54 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
+<<<<<<< HEAD
     new_array = []
     array.each do |element|
     new_array << element **2
   end
   new_array
+=======
+  array = [1,2,3,4,5]
+  square_array()
+>>>>>>> e49d8bc1787906546494dfa2bcc54b51f2dfe609
 end
